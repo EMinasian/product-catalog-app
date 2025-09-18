692406a4afbaab673dc01ad613d921986a197203
erik.minasian <eminasian@webmd.net>
2025-09-18 21:25:00 +0400

    feat: debouncing for search

--- src/component/SearchPanel.jsx
+++ src/component/SearchPanel.jsx
@@ -6,6 +6,19 @@ export default function SearchPanel() {
   const searchInput = useRef("");
   const { setFilter } = useContext(FilterContext);
 
+  const handleSearch = () => {
+    let timeoutId;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(
+      () =>
+        setFilter((prev) => ({
+          ...prev,
+          [NAME_SEARCH_KEY]: searchInput.current.value,
+        })),
+      1 * 2000
+    );
+  };
+
   return (
     <div className="flex bg-blue-950 text-amber-100 rounded-sm p-1 w-full m-1 justify-between">
       <label for={NAME_SEARCH_KEY}>Search product name:</label>
@@ -17,12 +30,7 @@ export default function SearchPanel() {
       />
       <button
         className="bg-green-600 px-2 py-1 text-black rounded-sm"
-        onClick={() => {
-          setFilter((prev) => ({
-            ...prev,
-            [NAME_SEARCH_KEY]: searchInput.current.value,
-          }));
-        }}
+        onClick={handleSearch}
       >
         Search
       </button>
