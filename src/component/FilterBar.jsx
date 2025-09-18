3e424423b073c417cc386625549fcb68e3124184
erik.minasian <eminasian@webmd.net>
2025-09-18 16:42:25 +0400

    feat: reset added

--- src/component/FilterBar.jsx
+++ src/component/FilterBar.jsx
@@ -1,59 +1,78 @@
-import { useState } from "react"
-import DiscreteFilter from "./DiscreteFilter"
-import RangeFilter from "./RangeFilter"
-import SortingPanel from "./SortingPanel"
-import { BRAND_KEY, CATEGORY_KEY, RATING_KEY, PRICE_KEY } from "../utils/consts"
+import { useState, useContext } from "react";
+import DiscreteFilter from "./DiscreteFilter";
+import RangeFilter from "./RangeFilter";
+import SortingPanel from "./SortingPanel";
+import { FilterContext } from "../contexts/FilterContext";
+import {
+  BRAND_KEY,
+  CATEGORY_KEY,
+  RATING_KEY,
+  PRICE_KEY,
+} from "../utils/consts";
 
 const DISCRETE_FILTER_VALUES = [
   {
-    id: BRAND_KEY, title: 'Brand', options: ['Brand A', 'Brand B', 'Brand C', 'Brand D', 'Brand E']
+    id: BRAND_KEY,
+    title: "Brand",
+    options: ["Brand A", "Brand B", "Brand C", "Brand D", "Brand E"],
   },
   {
-    id: CATEGORY_KEY, title: 'Category', options: ['Electronics', 'Footwear', 'Clothing']
-  }
-]
+    id: CATEGORY_KEY,
+    title: "Category",
+    options: ["Electronics", "Footwear", "Clothing"],
+  },
+];
 
 const RANGE_FILTER_VALUES = [
   {
-    id: RATING_KEY, title: 'Minimum Rating', min: 0, max: 5
+    id: RATING_KEY,
+    title: "Minimum Rating",
+    min: 0,
+    max: 5,
   },
   {
-    id: PRICE_KEY, title: 'Minimum Price', min: 0, max: 500
-  }
-]
+    id: PRICE_KEY,
+    title: "Minimum Price",
+    min: 0,
+    max: 500,
+  },
+];
 
-export default function FilterBar () {
+export default function FilterBar() {
+  const [openMobileFilter, setOpenMobileFilter] = useState(false);
 
-  const [openMobileFilter, setOpenMobileFilter] = useState(false)
+  const { handleReset } = useContext(FilterContext);
 
   return (
     <div className="h-12 w-full bg-amber-600 px-10">
       <div className="hidden md:flex">
-        {
-          DISCRETE_FILTER_VALUES.map(({ id, title, options }) => <DiscreteFilter id={id} title={title} options={options} />)
-        }
-        {
-          RANGE_FILTER_VALUES.map(({ id, title, max, min }) => <RangeFilter id={id} title={title} max={max} min={min} />)
-        }
+        {DISCRETE_FILTER_VALUES.map(({ id, title, options }) => (
+          <DiscreteFilter id={id} title={title} options={options} />
+        ))}
+        {RANGE_FILTER_VALUES.map(({ id, title, max, min }) => (
+          <RangeFilter id={id} title={title} max={max} min={min} />
+        ))}
         <SortingPanel />
+        <button onClick={handleReset}>Reset</button>
       </div>
-      <button className="md:hidden bg-blue-950 text-amber-50 p-2 m-1 rounded-sm" onClick={() => setOpenMobileFilter(true)}>Filters</button>
-      {
-        openMobileFilter && (
-          <div className="fixed top-0 left-0 flex flex-col bg-amber-600 w-[80%] gap-2 px-2 py-6">
-            {
-              DISCRETE_FILTER_VALUES.map(({ id, title, options }) => <DiscreteFilter id={id} title={title} options={options} />)
-            }
-            {
-              RANGE_FILTER_VALUES.map(({ id, title, max, min }) => <RangeFilter id={id} title={title} max={max} min={min} />)
-            }
-            <SortingPanel />
-            <button onClick={() => setOpenMobileFilter(false)}>close</button>
-          </div>
-        )
-      }
-
+      <button
+        className="md:hidden bg-blue-950 text-amber-50 p-2 m-1 rounded-sm"
+        onClick={() => setOpenMobileFilter(true)}
+      >
+        Filters
+      </button>
+      {openMobileFilter && (
+        <div className="fixed top-0 left-0 flex flex-col bg-amber-600 w-[80%] gap-2 px-2 py-6">
+          {DISCRETE_FILTER_VALUES.map(({ id, title, options }) => (
+            <DiscreteFilter id={id} title={title} options={options} />
+          ))}
+          {RANGE_FILTER_VALUES.map(({ id, title, max, min }) => (
+            <RangeFilter id={id} title={title} max={max} min={min} />
+          ))}
+          <SortingPanel />
+          <button onClick={() => setOpenMobileFilter(false)}>close</button>
+        </div>
+      )}
     </div>
-
-  )
+  );
 }
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,56 +1,103 @@
-import { useState } from "react";
+import { Suspense, useState, useTransition } from "react";
 import Product from "./Product";
 import FilterBar from "./FilterBar";
-import mockData from "../mock/data.json"
+import mockData from "../mock/data.json";
 import { FilterProvider } from "../contexts/FilterContext";
-import { BRAND_KEY, CATEGORY_KEY, RATING_KEY, PRICE_KEY, SORTING_CRITERIA_ID, SORTING_DIRECTION_ID, SORTING_ASCENDING } from "../utils/consts";
+import {
+  BRAND_KEY,
+  CATEGORY_KEY,
+  RATING_KEY,
+  PRICE_KEY,
+  SORTING_CRITERIA_ID,
+  SORTING_DIRECTION_ID,
+  SORTING_ASCENDING,
+} from "../utils/consts";
 
-const DISCRETE_FILTERS = [ BRAND_KEY, CATEGORY_KEY ]
-const RANGE_FILTERS = [ RATING_KEY, PRICE_KEY ]
+const DISCRETE_FILTERS = [BRAND_KEY, CATEGORY_KEY];
+const RANGE_FILTERS = [RATING_KEY, PRICE_KEY];
 
-export default function Table () {
-  const [filter, setFilter] = useState({[RATING_KEY]: 0, [PRICE_KEY]: 0})
-  const [sorting, setSorting] = useState({})
+export default function Table() {
+  const [filter, setFilter] = useState({ [RATING_KEY]: 0, [PRICE_KEY]: 0 });
+  const [sorting, setSorting] = useState({});
+
+  const [pendingReset, startResetTransition] = useTransition();
 
   const filterData = () => {
-    const discreteFilteredData = DISCRETE_FILTERS.reduce((accumulator, current) => {
-      if (filter[current]) {
-        return accumulator.filter(item => item[current] === filter[current])
-      }
-      return accumulator
-    }, mockData)
+    const discreteFilteredData = DISCRETE_FILTERS.reduce(
+      (accumulator, current) => {
+        if (filter[current]) {
+          return accumulator.filter(
+            (item) => item[current] === filter[current]
+          );
+        }
+        return accumulator;
+      },
+      mockData
+    );
 
     const rangeFilteredData = RANGE_FILTERS.reduce((accumulator, current) => {
       if (filter[current]) {
-        return accumulator.filter(item => item[current] >= filter[current])
+        return accumulator.filter((item) => item[current] >= filter[current]);
       }
-      return accumulator
-    }, discreteFilteredData)
+      return accumulator;
+    }, discreteFilteredData);
 
-    return rangeFilteredData
-  }
+    return rangeFilteredData;
+  };
 
-  const filteredData = filterData()
+  const filteredData = filterData();
 
   const sortData = () => {
     if (!sorting[SORTING_CRITERIA_ID] || !sorting[SORTING_DIRECTION_ID]) {
-      return filteredData
+      return filteredData;
     }
-    return filteredData.sort((a, b) => (a[sorting[SORTING_CRITERIA_ID]] - b[sorting[SORTING_CRITERIA_ID]]) * (sorting[SORTING_DIRECTION_ID] === SORTING_ASCENDING ? 1 : -1))
-  }
+    return filteredData.sort(
+      (a, b) =>
+        (a[sorting[SORTING_CRITERIA_ID]] - b[sorting[SORTING_CRITERIA_ID]]) *
+        (sorting[SORTING_DIRECTION_ID] === SORTING_ASCENDING ? 1 : -1)
+    );
+  };
 
-  const sortedData = sortData()
+  const sortedData = sortData();
+
+  const handleReset = () => {
+    startResetTransition(async () => {
+      // artificaial delay for loading purposes
+      await new Promise((r) => setTimeout(r, 2000));
+      setSorting({});
+      setFilter({});
+    });
+  };
 
   return (
-    <div className="flex flex-col items-center sizes-full bg-blue-950">
-      <FilterProvider value={{ filter, setFilter, sorting, setSorting }}>
-        <FilterBar />
-      </FilterProvider>
-      <div className="grid md:grid-cols-3 grid-cols-1 w-[90%] md:w-[70%] bg-blue-400 p-4 md:p-8 gap-8">
-        {sortedData.map(({ name, category, brand, price, rating, imageUrl }) => 
-          <Product name={name} category={category} brand={brand} price={price} rating={rating} imageUrl={imageUrl}/>)}
+    <Suspense fallback={<span>Loading</span>}>
+      <div className="flex flex-col items-center sizes-full bg-blue-950">
+        <FilterProvider
+          value={{ filter, setFilter, sorting, setSorting, handleReset }}
+        >
+          <FilterBar />
+        </FilterProvider>
+        <div className="grid md:grid-cols-3 grid-cols-1 w-[90%] md:w-[70%] bg-blue-400 p-4 md:p-8 gap-8">
+          {pendingReset ? (
+            <span>Loading!</span>
+          ) : sortedData.length === 0 ? (
+            <span>No products found!</span>
+          ) : (
+            sortedData.map(
+              ({ name, category, brand, price, rating, imageUrl }) => (
+                <Product
+                  name={name}
+                  category={category}
+                  brand={brand}
+                  price={price}
+                  rating={rating}
+                  imageUrl={imageUrl}
+                />
+              )
+            )
+          )}
+        </div>
       </div>
-    </div>
-
-  )
+    </Suspense>
+  );
 }
