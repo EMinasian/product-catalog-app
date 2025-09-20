17fb68dd4dfe78f6809c16972d3ba63ea37b927d
erik.minasian <eminasian@webmd.net>
2025-09-20 14:18:43 +0400

    feat: test pagination

--- src/tests/Table.test.jsx
+++ src/tests/Table.test.jsx
@@ -1,5 +1,6 @@
 import Table from "../component/Table";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 import React from "react";
@@ -16,5 +17,21 @@ describe("Table", () => {
 
     const paginationPanel = screen.getByTestId("products-panel");
     expect(paginationPanel).toBeInTheDocument();
+  });
+
+  test("Pagination", async () => {
+    render(<Table />);
+
+    const user = userEvent.setup();
+    const previousButton = screen.getByText("Previous");
+    console.log(previousButton);
+    expect(previousButton).toBeDisabled();
+
+    const nextButton = screen.getByText("Next");
+    await act(async () => user.click(nextButton));
+
+    const productsPanel = screen.getByTestId("products-panel");
+    expect(productsPanel.childNodes).toHaveLength(1);
+    expect(nextButton).toBeDisabled();
   });
 });
